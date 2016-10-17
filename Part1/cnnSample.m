d8ccecc4e2c1eeaf3a527e24f0dd244b93eee253
Dian Bakti <dbakti1605@gmail.com>
2016-10-17 15:59:34 +0800

    restructured

---
+++ Part1/cnnSample.m
@@ -0,0 +1,45 @@
+% cnnPreprocess();
+
+load 'dataTeststore.mat';
+load 'dataTrainstore.mat';
+
+% obsolete
+% dataTrainstoreSubset = imageDatastore('..\');
+% dataTeststoreSubset = imageDatastore('..\');
+
+dataTrainstoreSubset.Files = dataTrainstore.Files;
+dataTeststoreSubset.Files = dataTeststore.Files;
+
+dataTrainstoreSubset.Labels = dataTrainstore.Labels;
+dataTeststoreSubset.Labels = dataTeststore.Labels;
+
+imageDim = 28;
+
+layers = [...
+    imageInputLayer([imageDim imageDim])
+	 convolution2dLayer([9, 9],20)
+	 averagePooling2dLayer([2, 2])
+%      convolution2dLayer([5, 5],50), ...
+%      averagePooling2dLayer([2, 2]), ...
+     fullyConnectedLayer(10)
+	 softmaxLayer()
+     classificationLayer()];
+ 
+ options = trainingOptions('sgdm', ... 
+            'MaxEpochs', 25,...
+            'InitialLearnRate',0.001, ...
+            'L2Regularization', 0.001, ...
+            'Momentum', 0.9, ...
+            'MiniBatchSize', 16 ...
+        );
+
+disp('Training...');
+convnet = trainNetwork(dataTrainstoreSubset,layers,options);
+
+disp('Training Done...');
+
+disp('Testing...');
+YTest = classify(convnet, dataTeststoreSubset);
+TTest = dataTeststoreSubset.Labels;
+accuracy = sum(YTest == TTest)/numel(YTest);
+disp(accuracy);
